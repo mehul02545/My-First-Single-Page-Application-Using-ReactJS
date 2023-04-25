812bc98c18a73ebd9a09cd9960230492d4fc2dc0
mehul02545 <mehul1162@gmail.com>
2023-04-24 21:37:49 -0400

    Readme file updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return (
-      <footer className='bg-secondary text-center'>
+      <footer className='bg-secondary text-center' >
         <div className="footer-container p-4">&#169;2023 @blogpost.com All rights reserved.</div>
       </footer>
     );
